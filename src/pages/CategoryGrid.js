2491e951051b9130f1503951a1a23df53193608d
Durgakesav <dktipparaju@gmail.com>
2025-09-26 23:21:22 +0530

    third

--- src/pages/CategoryGrid.js
+++ src/pages/CategoryGrid.js
@@ -2,9 +2,46 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { categories } from "./categoriesData";
 
 export default function CategoryGrid() {
+  // categoriesData.js
+ const categories = {
+  "Electronics-Gadgets": [
+    "Pendrive",
+    "MemoryCard",
+    "SD Card",
+    "Phones",
+    "Watches",
+    "Money Counting Machine",
+    "Mouse",
+    "Radio",
+    "TV Remotes",
+    "AC Remotes",
+    "AC Stabilizer",
+    "TV Stabilizer",
+    "Phone Charger",
+    "Computer Accessories",
+    "Phone Accessories",
+    "Telephony / Landline",
+    "Digital Alarm",
+    "Wall Clocks",
+    "Vlogging Mic",
+    "Tripod Stand",
+    "Ring Light"
+  ],
+  "Home & Services": [
+    "Phone Glass",
+    "Phone Pouch",
+    "Phone Service",
+    "Water Heaters",
+    "Perfumes"
+  ],
+  "Home Decor": [
+    "Wall Clocks"
+  ]
+};
+
+
   const router = useRouter();
   const [imgError, setImgError] = useState({});
 
